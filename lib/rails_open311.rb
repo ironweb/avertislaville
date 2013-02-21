07ee16af342c4dc2e92dadec652dacd6fe9ca687
Gregory Eric Sanderson <gzou2000@gmail.com>
2013-02-21 00:24:04 -0500

    code style issues

--- lib/rails_open311.rb
+++ lib/rails_open311.rb
@@ -3,19 +3,21 @@ module RailsOpen311
   CONFIG_PATH = "#{Rails.root}/config/open311.yml"
   CACHE_KEY = 'open311_services'
 
+  def self.load_config
+    YAML.load_file(CONFIG_PATH)
+  end
+
   def self.load_all_services!
-    api_config = YAML.load_file(CONFIG_PATH)
+    api_config = load_config
 
     api_wrapper = Open311::ApiWrapper.from_url(api_config['url'])
     services = api_wrapper.services_with_attributes
 
     Rails.cache.write CACHE_KEY, services
-
   end
 
   def self.all_services
-    services = Rails.cache.read CACHE_KEY
-    return services
+    Rails.cache.read CACHE_KEY
   end
 
 end
