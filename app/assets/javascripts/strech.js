8986a46687ddc6944060c9666e47496bfb1d9b9a
Damien Van Der Windt <vdwdamien@gmail.com>
2013-02-22 08:13:45 -0500

    stretch services button

---
+++ app/assets/javascripts/strech.js
@@ -0,0 +1,17 @@
+function initStrech() {
+	if($('.services').length === 0) return;
+	var viewportHeight = $(window).height();
+	var headerHeight = $("header.navBar").height();
+	var heightContent = viewportHeight-headerHeight;
+	if(heightContent>320){
+		$(".services a").css({'height' : (heightContent/6), 'padding-top' : (heightContent/6)*2, 'background-position' : 'center '+((heightContent/6)-20)+"px"});
+		console.log("chaage");
+	}
+}
+
+$(document).ready(function() {
+	initStrech();
+	$(window).resize(function() {
+		initStrech();
+	});
+});
