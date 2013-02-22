27cfa399b56f9d63c10bd76de60f49f37f1eb3fc
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2013-02-22 03:54:45 -0500

    Retab + standardise formatting in js

--- app/assets/javascripts/localisation.js
+++ app/assets/javascripts/localisation.js
@@ -16,24 +16,24 @@ function updateCoordinate(){
 			adress = "Adresse indisponible";
 		}
 		$('#localisation').html(adress);
-    });
-    $("#request_lat").val(latitudeData);
-    $("#request_lon").val(longitudeData);
+	});
+	$("#request_lat").val(latitudeData);
+	$("#request_lon").val(longitudeData);
 }
 
-function initMap(){
+function initMap() {
 	if($('#map_canvas').length === 0) return;
 	$("#map_canvas").goMap({
 		maptype: 'ROADMAP',
 		mapTypeControl: false,
 		navigationControl: true,
-        zoom: zoomData,
-        markers: [{
-            latitude: latitudeData,
-            longitude: longitudeData,
-            draggable: true,
-            id: 'testMarker'
-        }]
+		zoom: zoomData,
+		markers: [{
+			latitude: latitudeData,
+			longitude: longitudeData,
+			draggable: true,
+			id: 'testMarker'
+		}]
 	});
 	geocoder = new google.maps.Geocoder();
 	updateCoordinate();
@@ -47,33 +47,31 @@ function initMap(){
 		latitudeData=split[0];
 		longitudeData=split[1];
 		updateCoordinate();
-    });
+	});
 
 	// At Click on map, move marker to position
-    $.goMap.createListener({type:'map'}, 'click', function(event) {
-        latitudeData=event.latLng.lat();
-        longitudeData=event.latLng.lng();
-        $.goMap.setMarker('testMarker',{latitude:latitudeData, longitude:longitudeData});
-        updateCoordinate();
-    });
+	$.goMap.createListener({type:'map'}, 'click', function(event) {
+		latitudeData=event.latLng.lat();
+		longitudeData=event.latLng.lng();
+		$.goMap.setMarker('testMarker',{latitude:latitudeData, longitude:longitudeData});
+		updateCoordinate();
+	});
 }
 
-function success_callback(p)
-{
+function success_callback(p) {
 	latitudeData = p.coords.latitude;
 	longitudeData = p.coords.longitude;
 	zoomData=18;
 	initMap();
 }
-	
-function error_callback(p)
-{
+
+function error_callback(p) {
 	console.log('error='+p.message);
 	initMap();
 }
 
-function initLocalisation(){
-	if(geo_position_js.init()){
+function initLocalisation() {
+	if(geo_position_js.init()) {
 		geo_position_js.getCurrentPosition(success_callback,error_callback,{enableHighAccuracy:true});
 	}
 	else{
@@ -82,12 +80,12 @@ function initLocalisation(){
 	}
 }
 
-function initBigMap(){
+function initBigMap() {
 	$("#enlarge").hide();
 	$("#minimize").show();
 	$("#map_canvas").animate({height: "350"});
 }
-function endBigMap(){
+function endBigMap() {
 	$("#minimize").hide();
 	$("#enlarge").show();
 	$("#map_canvas").animate({height: "200"});
