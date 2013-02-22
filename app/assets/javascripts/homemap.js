4c75aef74934e7d02b4125ea51d6500ef376c0bb
Damien Van Der Windt <vdwdamien@gmail.com>
2013-02-22 10:00:38 -0500

    Homepage Map

---
+++ app/assets/javascripts/homemap.js
@@ -0,0 +1,23 @@
+function initHomeMap (){
+	$("h1.logo").after("<div id='zoneHomeMap'><div id='homeMap'></div><div id='mask'></div></div>");
+	var objectMarker = eventsCoords.map(function(coord) {
+		return { latitude: coord.lat, longitude: coord.lon };
+	});
+	$("#homeMap").goMap({
+        navigationControl: false,
+        mapTypeControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        zoom: 12,
+        maptype: 'ROADMAP',
+        markers: objectMarker
+    });
+	$.goMap.setMap({latitude:'46.836944988044465', longitude:'-71.28376007080078'});
+}
+
+$(document).ready(function() {
+	if($(".controller-home").length === 0) return;
+	if($(window).width()>=1024){
+		initHomeMap();
+	}
+});
