868b6239d19b3526ee111dd1a3e0f2944c29aee4
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2013-02-22 12:50:58 -0500

    Allow map late initialization on home

--- app/assets/javascripts/homemap.js
+++ app/assets/javascripts/homemap.js
@@ -1,7 +1,7 @@
-function initHomeMap (){
+function initHomeMap (coords){
 	$("h1.logo").after("<div id='zoneHomeMap'><div id='homeMap'></div><div id='mask'></div></div>");
 	var imgmarker='../img/marker.png';
-	var objectMarker = eventsCoords.map(function(coord) {
+	var objectMarker = coords.map(function(coord) {
 		return { latitude: coord.lat, longitude: coord.lon, icon: imgmarker};
 	});
 	$("#homeMap").goMap({
@@ -16,9 +16,14 @@ function initHomeMap (){
 	$.goMap.setMap({latitude:'46.836944988044465', longitude:'-71.28376007080078'});
 }
 
-$(document).ready(function() {
-	if($(".controller-home").length === 0) return;
-	if($(window).width()>=1024){
-		initHomeMap();
-	}
+$(document).on('homemap:ready', function(e, coords) {
+	// Idempotence
+	if($("#zoneHomeMap").length !== 0) return;
+	$(window).on('resize.homemap', function() {
+		if($("#zoneHomeMap").length == 0 && $(window).width() > 960) {
+			initHomeMap(coords);
+		}
+	})
+	$(window).trigger('resize.homemap');
+	$(document).on('page:change', function() { $(window.off('.homemap')); });
 });
