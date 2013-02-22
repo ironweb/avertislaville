57b7b6959cce47e28cd383748c250cfeaf3080a4
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2013-02-22 14:13:00 -0500

    Improve services styling

--- app/assets/javascripts/strech.js
+++ app/assets/javascripts/strech.js
@@ -3,7 +3,10 @@ function initStrech() {
 	var viewportHeight = $(window).height();
 	var headerHeight = $("header.navBar").height();
 	var heightContent = viewportHeight-headerHeight;
-	if(heightContent>320){
+	if($(window).width() >= 1024) {
+		$(".services a").removeAttr('style');
+	}
+	else if(heightContent>320){
 		$(".services a").css({'height' : (heightContent/6), 'padding-top' : (heightContent/6)*2, 'background-position' : 'center '+((heightContent/6)-20)+"px"});
 	}
 }
